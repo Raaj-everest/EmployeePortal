805c5b3f61525fcd77c83f43a17e0f0a6b6be429
rajesh <raaj.everest@gmail.com>
2022-02-16 15:14:33 +0530

    added implementation to delete employees

--- src/main/java/com/everest/employeePortal/controller/EmployeeRestController.java
+++ src/main/java/com/everest/employeePortal/controller/EmployeeRestController.java
@@ -2,6 +2,10 @@ package com.everest.employeePortal.controller;
 
 import com.everest.employeePortal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +16,10 @@ public class EmployeeRestController {
 
     private final EmployeeService employeeService;
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteEmployee(@PathVariable Long id) {
+        employeeService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 
 }
--- src/main/java/com/everest/employeePortal/services/EmployeeService.java
+++ src/main/java/com/everest/employeePortal/services/EmployeeService.java
@@ -1,10 +1,13 @@
 package com.everest.employeePortal.services;
 
+import com.everest.employeePortal.entities.Employee;
+import com.everest.employeePortal.exceptions.EmployeeNotFoundException;
 import com.everest.employeePortal.repositories.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -13,4 +16,11 @@ public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
+    public void delete(Long id) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if (employee.isEmpty()) {
+            throw new EmployeeNotFoundException("NO Employee Found with given Id :" + id + "perhaps he is not exists ");
+        }
+        employeeRepository.delete(employee.get());
+    }
 }
