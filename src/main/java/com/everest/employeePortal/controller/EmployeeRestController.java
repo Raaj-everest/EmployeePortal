5218f00794556b9007ba3f38eb4b3dd76fe99237
rajesh <raaj.everest@gmail.com>
2022-02-16 14:45:08 +0530

    added implementation for updating employee

--- src/main/java/com/everest/employeePortal/controller/EmployeeRestController.java
+++ src/main/java/com/everest/employeePortal/controller/EmployeeRestController.java
@@ -1,9 +1,9 @@
 package com.everest.employeePortal.controller;
 
+import com.everest.employeePortal.entities.Employee;
 import com.everest.employeePortal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,5 +12,8 @@ public class EmployeeRestController {
 
     private final EmployeeService employeeService;
 
-
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        return employeeService.update(employee, id);
+    }
 }
--- src/main/java/com/everest/employeePortal/services/EmployeeService.java
+++ src/main/java/com/everest/employeePortal/services/EmployeeService.java
@@ -1,10 +1,12 @@
 package com.everest.employeePortal.services;
 
+import com.everest.employeePortal.entities.Employee;
 import com.everest.employeePortal.repositories.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -13,4 +15,13 @@ public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
+
+    public Employee update(Employee newEmployee, Long id) {
+        Optional<Employee> oldEmployee = employeeRepository.findById(id);
+        if (oldEmployee.isEmpty()) {
+            throw new EmployeeNotFoundException("No employee found with given ID : " + id);
+        }
+        newEmployee.setId(id);
+        return employeeRepository.save(newEmployee);
+    }
 }
