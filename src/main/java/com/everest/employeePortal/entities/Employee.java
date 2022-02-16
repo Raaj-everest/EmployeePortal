3e348d0d06d342f5ad16e3d501035658966cf19c
rajesh <raaj.everest@gmail.com>
2022-02-16 12:30:13 +0530

    added boilerPlate code added entities,repositories,services and controllers. succesfully connected to database

---
+++ src/main/java/com/everest/employeePortal/entities/Employee.java
@@ -0,0 +1,56 @@
+package com.everest.employeePortal.entities;
+
+import com.everest.employeePortal.entities.enums.Designation;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "date_of_join", nullable = false)
+    private LocalDate dateOfJoin;
+
+    @Column(nullable = false)
+    private Designation designation;
+
+    @Column(name = "company_mail", nullable = false)
+    private String everestEmail;
+
+    @Column(name = "personal_mail")
+    private String personalEmail;
+
+    @Column
+    private String bio;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "present_address_id")
+    private Address presentAddress;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "permanent_address_id")
+    private Address permanentAddress;
+
+}
---
+++ src/test/java/com/everest/employeePortal/EmployeePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everest.employeePortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
