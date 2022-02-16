7e6fb0efbf38b99bbe1212044e3f292b69126f49
rajesh <raaj.everest@gmail.com>
2022-02-16 14:06:51 +0530

    added implementation to get employees

--- src/main/java/com/everest/employeePortal/controller/EmployeeRestController.java
+++ src/main/java/com/everest/employeePortal/controller/EmployeeRestController.java
@@ -1,9 +1,14 @@
 package com.everest.employeePortal.controller;
 
+import com.everest.employeePortal.entities.Employee;
 import com.everest.employeePortal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,5 +17,13 @@ public class EmployeeRestController {
 
     private final EmployeeService employeeService;
 
+    @GetMapping("")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAll();
+    }
 
+    @GetMapping("/{id}")
+    public Employee getEmployeeByID(@PathVariable Long id) {
+        return employeeService.getByID(id);
+    }
 }
--- src/main/java/com/everest/employeePortal/services/EmployeeService.java
+++ src/main/java/com/everest/employeePortal/services/EmployeeService.java
@@ -1,10 +1,14 @@
 package com.everest.employeePortal.services;
 
+import com.everest.employeePortal.entities.Employee;
+import com.everest.employeePortal.exceptions.EmployeeNotFoundException;
 import com.everest.employeePortal.repositories.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -13,4 +17,15 @@ public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
+    public List<Employee> getAll() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getByID(Long id) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if (employee.isEmpty()) {
+            throw new EmployeeNotFoundException("No Employee found with given ID :" + id);
+        }
+        return employee.get();
+    }
 }
