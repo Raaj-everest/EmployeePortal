3ef8fc0903c6125875e48a5178583db090d3b6a0
rajesh <raaj.everest@gmail.com>
2022-02-21 11:40:44 +0530

    adde transactional annotation from java.org.spring

--- src/main/java/com/everest/employeePortal/services/EmployeeService.java
+++ src/main/java/com/everest/employeePortal/services/EmployeeService.java
@@ -4,8 +4,7 @@ import com.everest.employeePortal.entities.Employee;
 import com.everest.employeePortal.repositories.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
